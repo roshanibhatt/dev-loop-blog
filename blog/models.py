5f0b2e15e504e3eac83d27dfa2dcb87e3f6ddd9d
Roshani Bhatt <roshanibhatt98@gmail.com>
2025-02-02 16:50:28 +0545

    commit2

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Blog (models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=200)
-    Content=models.TextField()
+    Content=HTMLField()
     slug=models.CharField(max_length=100)
     time=models.DateTimeField(auto_now_add=True)
 
